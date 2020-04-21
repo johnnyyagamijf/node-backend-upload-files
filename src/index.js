c85e61bc81912a16b7933515f2ea9c4d77965764
jmdc <johnmdcampos@gmail.com>
2020-04-21 17:28:50 -0300

    mudança no bd

--- src/index.js
+++ src/index.js
@@ -10,13 +10,11 @@ mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 });
-//mongodb+srv://bd_user:bduser@cluster0-02jjc.mongodb.net/upload?retryWrites=true&w=majority
 
 app.use(express.json());
 app.use(express.urlencoded( { extended : true }));
 app.use(morgan('dev'));
 app.use('/file', express.static(path.resolve(__dirname,'..', 'tmp', 'uploads')))
 app.use(require('./routes'))
-//mongodb+srv://bd_user:<password>@cluster0-02jjc.mongodb.net/test?retryWrites=true&w=majority
 
 app.listen(4000);
